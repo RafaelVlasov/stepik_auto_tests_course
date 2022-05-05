77c7ceed287191323cb176e149c756de722417af
RafaelVlasov <rafael.vlasov@gmail.com>
2022-05-05 16:35:29 +0300

    add small changes

--- test_pet_project1/conftest.py
+++ test_pet_project1/conftest.py
@@ -29,7 +29,6 @@ def browser(request):
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
-        #browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
--- test_pet_project1/test_items.py
+++ test_pet_project1/test_items.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium import webdriver
-import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
@@ -8,4 +7,3 @@ def test_find_button_transfer_product_to_basket(browser):
     browser.get(link)
     button = browser.find_elements_by_class_name("btn-add-to-basket")
     assert button, "button missing"
-    time.sleep(5)
